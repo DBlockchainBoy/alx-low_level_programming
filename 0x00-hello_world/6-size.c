b8995a409f09f68940fe4b9445c4c98574da4c27
DBlockChainBoy <techplugz91@gmail.com>
2022-12-09 14:25:41 -0800

    Add code for task 6 of C-Hello,world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an interger: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long integer: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a lomg long integer: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
+}
