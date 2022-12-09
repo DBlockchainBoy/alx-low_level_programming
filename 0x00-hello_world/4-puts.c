ba0080b980e0ea12d34424ffd682cb857d9458f3
DBlockChainBoy <techplugz91@gmail.com>
2022-12-09 13:31:39 -0800

    Updated code for task 4 of C-Hello,world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
-/**
- * This program prints text on the
- * screen using puts
- */
 #include <stdio.h>
-#include <string.h>
-
 /**
- * main: takes no argument and 
- * returns an integer.
- * char: takes character input
- * puts: prints input from char
- * return: returns 0
+ * main- Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	char demo_char[] = "\"Programming is like building a multilingual puzzle";
-	puts(demo_char);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
