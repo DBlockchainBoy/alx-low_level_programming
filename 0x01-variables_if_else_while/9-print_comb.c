d79884d5ff440abd96ad5c70769066446044893b
DBlockChainBoy <techplugz91@gmail.com>
2022-12-12 03:10:50 -0800

    Updated the program that prints single digits numbers separated with comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,14 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(num);
-		putchar(',');
-		putchar(' ');
+		{
+			putchar(num);
+		}
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
