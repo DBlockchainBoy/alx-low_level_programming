9890e0761022c22436a70c68dffe45667578d8d8
DBlockChainBoy <techplugz91@gmail.com>
2022-12-12 01:48:44 -0800

    This program will print base 10 number digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Return:Always 0
+ */
+int main(void)
+{
+	int base10_num;
+
+	for (base10_num = 0; base10_num <= 9; base10_num++)
+	{
+		printf("%d ", base10_num);
+	}
+	printf("\n");
+	return (0);
+}
