68d3686ec8e9b8f91fa194e0fc9d8e7c5a47696e
DBlockChainBoy <techplugz91@gmail.com>
2022-12-14 11:45:56 -0800

    Added source code for the function that checks if an alphabet was entered

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha- Checks if a character is an alphabet i.e lower/ uppercase
+ * @c: Character to be checked
+ * Return: 1 on success and 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
