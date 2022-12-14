780af0f7a2c1c7bf53605009a8a980c10bd3584d
DBlockChainBoy <techplugz91@gmail.com>
2022-12-14 13:10:09 -0800

    Yet another update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,13 @@ int print_last_digit(int num)
 
 	last_digit = num % 10;
 	if (last_digit < 0)
-		last_digit *= -1;
-	_putchar(last_digit + '0');
-	return (0);
+	{
+		_putchar(-1 + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(1 + 48);
+		return (1);
+	}
 }
