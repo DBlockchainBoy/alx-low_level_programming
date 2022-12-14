4fb00fc0bf378a448016eb764ef527fc2bc7cbb1
DBlockChainBoy <techplugz91@gmail.com>
2022-12-14 12:29:49 -0800

    Added source code for the function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs- Computes the absolute value of an integer
+ * @r: integer to be computed
+ * Return: The absolute value of an integer
+ */
+int _abs(int r)
+{
+	if (r < 0)
+		return (-r);
+	else
+		return (r);
+}
