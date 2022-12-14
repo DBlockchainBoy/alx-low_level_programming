12b7804122265611a8abfb6badf262d947a336e4
DBlockChainBoy <techplugz91@gmail.com>
2022-12-14 11:11:13 -0800

    Made some correction in the last code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,9 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (alph = 'a'; alph <= 'z'; alph++)
-		_putchar(alph);
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
